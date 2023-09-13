868ce8ae95aa2a9b952aac20e4dc606a7d0bf752
AMALGSOFT <rivangushu200241>
2023-09-13 18:45:31 +0300

    first commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -84,10 +84,20 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '3EocjhAX5KuGpleJqBcH',
+        'HOST': 'containers-us-west-107.railway.app',
+        'PORT': '6005',
     }
 }
 
