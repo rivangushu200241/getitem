658ac446f9869738be24d764192c0c09d6228585
AMALGSOFT <rivangushu200241>
2023-09-29 13:45:39 +0300

    first commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_qqcv6!huk2hj!e$&meiq5nh@)a%#ev5e!=zsg1u_=y+sl=88l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
  
 ALLOWED_HOSTS = ['.vercel.app','*']
 
@@ -128,14 +128,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-in'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
+#Asia/Kolkata
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -159,4 +159,4 @@ CLOUDINARY_STORAGE ={
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-FILE_UPLOAD_MAX_MEMORY_SIZE = 10*1024*1024
+
